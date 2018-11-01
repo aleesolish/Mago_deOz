56a0980f624185c858a70d30b2d7e7d1c1f66484
aleesolish <aleesolish@gmail.com>
2018-11-01 13:38:58 -0600

    Cambio entre escenas
    Ya tiene el checkpoint para hacer el cambio de escenas

---
+++ Mago de oz/Assets/Scripts/Pases/Pase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pase : MonoBehaviour {
+
+    public GameObject cambio;
+
+	// Use this for initialization
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+        other.transform.position = cambio.transform.position; 
+	}
+}
---
+++ Mago de oz/Assets/Scripts/Pases/PaseEscena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PaseEscena : MonoBehaviour
+{
+    
+
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene("Stage1_2");
+    }
+}

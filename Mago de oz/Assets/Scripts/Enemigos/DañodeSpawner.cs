3b6522bbbb124a2e226b3036e7eac7c07568c948
aleesolish <aleesolish@gmail.com>
2018-11-08 14:15:44 -0600

    Health Bar Stage 2

--- Mago de oz/Assets/Scripts/Enemigos/DañodeSpawner.cs
+++ Mago de oz/Assets/Scripts/Enemigos/DañodeSpawner.cs
@@ -1,24 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DañodeSpawner : MonoBehaviour {
     [SerializeField]
     private float dano = .5f;
     private PlayerHealth player;
+    private Slider slider;
 
 	private void Start()
 	{
         player = FindObjectOfType<PlayerHealth>();
+        slider = player.HealthSlider;
 	}
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-        if (other.gameObject.CompareTag("Player")){
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
             player.currentHealth -= dano;
+            slider.value = player.currentHealth;
             Destroy(gameObject);
         }
-	}
+    }
 
 
 

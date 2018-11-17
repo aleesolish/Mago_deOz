1de5f59a9f47f21fa62fe15cf0728fe1a3d2ca7b
aleesolish <aleesolish@gmail.com>
2018-11-17 10:05:07 -0600

    Vida
    Ya hay objetos que te suben la vida

---
+++ Mago de oz/Assets/Scripts/Player/Vida.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vida : MonoBehaviour {
+    private int value = 5;
+    public PlayerHealth player;
+    public Slider HealthSlider;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+        if (other.CompareTag("Player")) {
+            player.currentHealth += value;
+            HealthSlider.value = player.currentHealth;
+            Destroy(gameObject);
+        }
+
+            
+	}
+
+	private void Start()
+	{
+        HealthSlider = player.HealthSlider;
+	}
+
+}

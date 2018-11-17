f36b053138eec6ed7f363f0384f1752f7b8b6759
aleesolish <aleesolish@gmail.com>
2018-11-17 10:42:17 -0600

    Conectar barra de poder con golpe

---
+++ Mago de oz/Assets/Scripts/Golpe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Golpe : MonoBehaviour {
+    public Slider coraje;
+    public GameObject punch;
+    private float waitTime = 0.6f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine("Punch"); 
+        }
+	}
+
+    IEnumerator Punch(){
+        punch.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(waitTime);
+        punch.gameObject.SetActive(false);
+
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        if (collision.CompareTag("Deadly")){
+            Destroy(collision.gameObject);
+            coraje.value += 3;
+        }
+	}
+}
